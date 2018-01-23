33bb52f052af881a0a6fc2872150caf3f3534f17
MartinCservenka <mhcservenka@gmail.com>
2018-01-23 22:03:33 +0100

    Updated readme.md and main file

--- README.md
+++ README.md
@@ -5,10 +5,29 @@
 The compiler is to be considered proof-of-concept in connection with my Master's Thesis on the ROOPL++ language.
 
 ## Requirements
+ROOPLPPC uses [Stack](https://docs.haskellstack.org/en/stable/README/) to manage all dependencies and requirements.
 
 ## Building
+Simply invoke
+```
+stack build
+```
+which compiles an executable into the `.stack-work` folder
 
 ## Usage
+To compile a ROOPL++ program simply run
+```
+stack exec ROOPLPPC input.rplpp
+```
+which compiles the input program into Pisa and stores the compiled file as `input.pal` in the current directory.
+
+To specify an output file name, simply provide it as an additional argument
+```
+stack exec ROOPLPP input.rplpp output.pal
+```
 
 ## Examples
+To see usage examples, please refer to `test/` for example programs. 
 
+## Running compiled programs
+The PendVM simulator executes compiled Pisa code and is hosted on Github [here](https://github.com/TueHaulund/PendVM).
--- src/ROOPLPPC.hs
+++ src/ROOPLPPC.hs
@@ -1,36 +1,34 @@
 import Control.Monad.Except
 import System.IO
+import System.Environment
 
 import PISA
 import Parser
 import ClassAnalyzer
--- import VectorAnalyzer
 import ScopeAnalyzer
 import TypeChecker
 import CodeGenerator
 import MacroExpander
 
+import Data.List.Split
+
 type Error = String
 
 main :: IO ()
 main =
-    do handle <- openFile "../test/Example.rplpp" ReadMode
+    do args <- getArgs
+       when (null args) (error "Supply input filename.\nUsage: ROOPLPPC input.rplpp output.pal\n")
+       when (length args > 2) (error "Too many arguments.\nUsage: ROOPLPPC input.rplpp output.pal\n")
+       handle <- openFile (head args) ReadMode
        input <- hGetContents handle
---        either (hPutStrLn stderr) printCAState (compileProgram input)
---        either (hPutStrLn stderr) printSAState (compileProgram input)
---        either (hPutStrLn stderr) showPISAProgram (compileProgram input)
---        either (hPutStrLn stderr) (putStr . showProgram) (compileProgram input)
-       either (hPutStrLn stderr) writeProgram (compileProgram input)
+       let output =  if length args == 2 then last args else head (splitOn "." (head args)) ++ ".pal"
+       either (hPutStrLn stderr) (writeProgram output) $ compileProgram input
 
--- compileProgram :: String -> Either Error (AST.Program, CAState)
--- compileProgram :: String -> Either String (SProgram, SAState)
--- compileProgram :: String -> Either String (PISA.MProgram, SAState)
 compileProgram :: String -> Either Error PISA.Program
 compileProgram s =
     runExcept $
     parseString s
     >>= classAnalysis
---     >>= vectorAnalysis
     >>= scopeAnalysis
     >>= typeCheck
     >>= generatePISA
